5106a05db7f48085a8bb09703136d3097afcb9e7
tostos <saittosun2@gmail.com>
2020-08-19 17:11:43 +0200

    more operators & more core data types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,40 +2,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// gets input from input field
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// generates and writes calculation log
 function createANumber(operator, initialNumber, currentNumber) {
   const calResult = `${initialNumber} ${operator} ${currentNumber}`;
-  outputResult(currentResult, calResult);
+  outputResult(currentResult, calResult); // from vendor file
 }
 
 function add() {
   const initialResult = currentResult;
   const enteredNumber = getUserNumberInput();
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber; 
+  // alert(++currentResult); //  I get the result after the change
+  // alert(currentResult++); //  I get the result before the change
   createANumber('+', initialResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createANumber('-', initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createANumber('*', initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createANumber('/', initialResult, enteredNumber);
 }
 
