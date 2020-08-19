af20316f5a2abb3da706f7ddc03f87dad34e4d67
tostos <saittosun2@gmail.com>
2020-08-19 11:27:26 +0200

    connecting all buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,43 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// function createANumber(operator, initialNumber, currentNumber) {
+//   const calResult = `${operator} ${initialNumber} ${currentNumber}`;
+//   outputResult(currentResult, calDescription);
+// }
+
 function add() {
   const enteredNumber = getUserNumberInput();
-  // const enteredNumber = parseInt(userInput.value);
   const calDescription = `${currentResult} + ${enteredNumber}`;
   currentResult = currentResult + enteredNumber;
   outputResult(currentResult, calDescription);
 }
 
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const calDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calDescription);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const calDescription = `${currentResult} * ${enteredNumber}`;
+  currentResult = currentResult * enteredNumber;
+  outputResult(currentResult, calDescription);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const calDescription = `${currentResult} / ${enteredNumber}`;
+  currentResult = currentResult / enteredNumber;
+  outputResult(currentResult, calDescription);
+}
+
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
