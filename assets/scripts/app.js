53e83096a827522f5f46daeff2af1868bfde647f
tostos <saittosun2@gmail.com>
2020-08-19 09:25:43 +0200

    converting data types & splitting code into functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  // const enteredNumber = parseInt(userInput.value);
+  const calDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calDescription);
 }
 
 addBtn.addEventListener('click', add);
